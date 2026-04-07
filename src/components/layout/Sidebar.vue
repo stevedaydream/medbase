8aaf870c2c46bdbc40b9712b1167b71577771eda
stevedaydream <tenant389@gmail.com>
2026-04-08 01:41:29 +0800

    chore: bump version to 0.1.7

--- src/components/layout/Sidebar.vue
+++ src/components/layout/Sidebar.vue
@@ -187,7 +187,7 @@ onUnmounted(() => {
     </div>
 
     <!-- 版本號 -->
-    <div class="px-4 py-3 text-xs text-gray-700">v0.1.6</div>
+    <div class="px-4 py-3 text-xs text-gray-700">v0.1.7</div>
 
     <!-- Drag ghost -->
     <Teleport to="body">
