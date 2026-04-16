b0c7cc5d26111f1619b4d3d0db410900ab61567f
stevedaydream <tenant389@gmail.com>
2026-04-16 08:19:27 +0800

    chore: bump version to 0.2.2

--- src/components/layout/Sidebar.vue
+++ src/components/layout/Sidebar.vue
@@ -200,7 +200,7 @@ onUnmounted(() => {
         <span class="text-sm">⇥</span>
         <span>精簡模式</span>
       </button>
-      <span class="ml-auto text-xs text-gray-700">v0.2.1</span>
+      <span class="ml-auto text-xs text-gray-700">v0.2.2</span>
     </div>
 
     <!-- Drag ghost -->
